1d2a8235ad73135725fe0d7e4e0603c022110cd9
MERZISEN Hugo <hugo.merzisen@meteo.fr>
2024-03-08 08:55:02 +0100

    updated some tasks

---
+++ tasks/20240308_downscale_season_end_season.py
@@ -0,0 +1,54 @@
+import rioxarray
+import os
+import xarray as xr
+import numpy as np
+import gc
+
+from bias_correction.postprocessing.arome_interpolation import get_arome_downscaled_loop, get_arome_interpolated, compute_epsg_2154
+
+
+previous_arome_dir="/cnrm/mrns/users/NO_SAVE/merzisenh/extraction_matthieu_v_2021/HendrixExtraction_2023_11_29_11_ID_1a9b1/alp/"
+arome_dir="/cnrm/mrns/users/NO_SAVE/merzisenh/extraction_matthieu_v_2021/HendrixExtraction_2024_02_07_00_ID_0e087/alp/"
+data_dir = "/home/merzisenh/NO_SAVE/arome_2021_2022_downscaled_devine"
+
+
+unet_output = xr.open_dataset(os.path.join(
+    data_dir, 'unet_output_on_small_dem.nc'))
+small_dem = xr.open_dataarray(os.path.join(
+    data_dir, "domaine_grande_rousse_small.nc"))
+# itérateur sur les fichiers arome renvoie -> arome_path, period
+# ouh c'est laid! tous les fichiers arome n'ont pas de lon/lat on prend celui là parce qu'on a constaté qu'il en a...
+arome_reference_for_lonlat = xr.open_dataset(
+    os.path.join(previous_arome_dir, "AROME_alp_2022_12.nc"))
+x_var, y_var = compute_epsg_2154(arome_reference_for_lonlat)
+
+
+list_periods = ["2022_07", "2022_08"]
+
+for period in list_periods:
+    filepath = os.path.join(arome_dir, f"AROME_alp_{period}.nc")
+    interpolated_arome_fullpath = os.path.join(
+        data_dir, "interpolated", f"arome_interpolated_completion_{period}.nc")
+    downscaled_arome_filepath = os.path.join(
+        data_dir, "downscaled", f"arome_downscaled_completion_{period}.nc")
+    if not os.path.exists(downscaled_arome_filepath):
+        if not os.path.exists(interpolated_arome_fullpath):
+            print(f"Interpolating Arome winds for period {period}")
+            arome = xr.open_dataset(filepath)
+            arome['x'] = x_var
+            arome['y'] = y_var
+            arome_interpolated = get_arome_interpolated(arome, small_dem)
+            arome_interpolated.to_netcdf(interpolated_arome_fullpath)
+            del arome
+        arome_interpolated = xr.open_dataset(interpolated_arome_fullpath)
+        print(f"Downscaling Arome winds for period {period}")
+        windspeed, direction = get_arome_downscaled_loop(
+            arome_interpolated,
+            unet_output
+        )
+        downscaled_wind = xr.Dataset(
+            {'speed': windspeed, 'direction': direction})
+        downscaled_wind.to_netcdf(downscaled_arome_filepath)
+        del downscaled_wind, windspeed, direction
+    del arome_interpolated
+    gc.collect()
